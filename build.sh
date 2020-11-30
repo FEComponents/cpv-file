4e930e13ce237ba14eab6e23fd10c9c7a32b0c0d
yuwangi <yuwangifeng@163.com>
2020-11-30 17:03:07 +0800

    fix: 配置

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-# yarn stdver
+###
+ # @Author: ywuangi
+ # @LastEditTime: 2020-11-30 16:02:21
+ # @LastEditors: your name
+ # @Description: 
+### 
+yarn stdver
 
 yarn build
--- notify.sh
+++ notify.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # https://stackoverflow.com/questions/13872048/bash-script-what-does-bin-bash-mean
 echo "1/5: checking TRAVIS_TEST_RESULT"
-echo $TRAVIS_TEST_RESULT
 if [ "$TRAVIS_TEST_RESULT" != "0" ]
 then
   echo "build not success, bye"
@@ -13,11 +12,11 @@ REPO_NAME=$(echo "$TRAVIS_REPO_SLUG" | cut -d '/' -f 2)
 
 echo "2/5: pushing commit and tag to github"
 # 该命令很可能报错，但不影响实际进行，因而不能简单地在脚本开头 set -e
-git remote add github https://d736bb2958e912593eefe9424e683774005a563e@github.com/$TRAVIS_REPO_SLUG.git > /dev/null 2>&1
+git remote add github https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git > /dev/null 2>&1
 git push github HEAD:main --follow-tags
 
 echo "3/5: generating github release notes"
-GREN_GITHUB_TOKEN=d736bb2958e912593eefe9424e683774005a563e yarn release
+GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn release
 
 # 避免发送错误信息
 if [ $? -ne 0 ]
@@ -30,7 +29,7 @@ echo "4/5: downloading github release info"
 url=https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases/latest
 resp_tmp_file=resp.tmp
 
-curl -H "Authorization: token d736bb2958e912593eefe9424e683774005a563e" $url > $resp_tmp_file
+curl -H "Authorization: token $GITHUB_TOKEN" $url > $resp_tmp_file
 
 html_url=$(sed -n 5p $resp_tmp_file | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}')
 body=$(grep body < $resp_tmp_file | sed 's/\"body\"://g;s/\"//g')
